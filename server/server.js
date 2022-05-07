8be8236d9d2e9eab044d6325e03ab44c88ef5792
mabus <m.buscaroli@outlook.com>
2022-05-07 21:05:46 +0100

    worked on server/server.js, there are now three routes: GET /, GET /smurfs, POST /smurfs

--- server/server.js
+++ server/server.js
@@ -18,3 +18,35 @@ app.get('/', (req, res) => {
 app.get('/smurfs', (req, res) => {
   res.send(dataArray)
 })
+
+app.post('/smurfs/', (req, res) => {
+  // we create a new variable with the name Clumsy
+  const newSmurf = 'Clumsy'
+
+  // We check if the name alrady exists in the database
+  // At the moment we have hardcoded the name so we'll be able to add it only once
+  const newSmurfExists = dataArray.every((smurf) => smurf !== newSmurf)
+
+  // If the newSmurf is already in the Database we are going to console.log rthe message
+  // and we are also sending a response with an HTTP Status of 405 (Forbidden) and a
+  // json object with a key of error and a value of errorMessage
+  if (newSmurfExists) {
+    const errorMessagesArray = ['Whoops!', 'Golly!', 'Gosh!']
+    numberOfMessages = errorMessagesArray.length
+
+    const randomIndex = Math.floor(Math.random() * numberOfMessages)
+    const errorMessage =
+      errorMessagesArray[randomIndex] + ' I am already in the SmurfDatabase!'
+
+    console.log(`${errorMessage} I am already in the SmurfDatabase!`)
+    res.status(403).send({
+      error:
+        errorMessagesArray[randomIndex] + ' I am already in the SmurfDatabase!',
+    })
+
+    // If the newSmurf is not in the db we can add them
+  } else {
+    dataArray.push(newSmurf)
+    res.send(newSmurf)
+  }
+})
